76f5215b6c0a7b228ed1a9d7f4a2f145504e67dd
wilmafernandes1993 <fwilma10@gmail.com>
2025-03-26 22:01:35 +0530

    Added the cors annotation

--- src/main/java/in/bushansirgur/restapi/controller/ExpenseController.java
+++ src/main/java/in/bushansirgur/restapi/controller/ExpenseController.java
@@ -6,6 +6,7 @@ import in.bushansirgur.restapi.service.ExpenseService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,7 @@ import java.util.stream.Collectors;
 @RestController
 @RequiredArgsConstructor
 @Slf4j
+@CrossOrigin("*")
 public class ExpenseController {
 
     private final ExpenseService expenseService;
