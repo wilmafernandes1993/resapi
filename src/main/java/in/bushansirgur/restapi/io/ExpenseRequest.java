33d7994e79eb0afa04d7cbaf028244325786337c
wilmafernandes1993 <fwilma10@gmail.com>
2025-03-29 13:31:43 +0530

    Completed save expense feature

--- src/main/java/in/bushansirgur/restapi/io/ExpenseRequest.java
+++ src/main/java/in/bushansirgur/restapi/io/ExpenseRequest.java
@@ -1,9 +1,9 @@
 package in.bushansirgur.restapi.io;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.sql.Date;
@@ -13,9 +13,19 @@ import java.sql.Date;
 @NoArgsConstructor
 @Builder
 public class ExpenseRequest {
+
+    @NotBlank(message = "Expense name is required")
+    @Size(min = 3, message= "Expense name should be atleast 3 charcaters")
     private String name;
+
     private String note;
+
+    @NotBlank(message = "Expense category is required")
     private String category;
+
+    @NotNull(message = "Expense date is required")
     private Date date;
+
+    @NotNull(message = "Expense amount is required")
     private BigDecimal amount;
 }
